bbf7f46268c4eabf4d4e7814e5cb159eacd243b3
Hassan Sani <saniyhassan@gmail.com>
2020-03-10 02:57:35 +0100

    done with routes for user service

--- services/user/src/routes/index.ts
+++ services/user/src/routes/index.ts
@@ -1,9 +1,30 @@
-import express from "express";
-// import { member } from "../controller";
+import express, { Router } from "express";
+import { member } from "../controller";
 
-const routes = express.Router();
+const routes: Router = express.Router();
 
-routes.all('/', function(req, res) {
-    res.status(200).json({message: `You are on User Service ADP`});
+routes.all('/', function (req, res) {
+    res.status(200).json({ message: `You are on User Service ADP` });
 });
+
+// Get all User
+routes.route('/user')
+    .get(member.getAllUser);
+
+// User by ID
+routes.route('/user/:id')
+    .get(member.getUserById)
+    .put(member.updateUser)
+    .delete(member.deactivateUser);
+
+// Get user by Email
+routes.route('/user/email/:user')
+    .get(member.getUserByEmail);
+
+// Reset user password
+routes.route('/reset/:id')
+    .get(member.requestPassword)
+    .put(member.checkReset)
+    .post(member.confirmReset);
+
 export default routes;
